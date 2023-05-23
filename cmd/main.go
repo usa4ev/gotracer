33781ac05bc66d256ebea8fb4d3633bdbd99626e
Pavel Usachev <usa4ev@gmail.com>
2023-05-23 21:28:40 +0300

    close mongo connection

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"context"
 	"flag"
-	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/sirupsen/logrus"
-	
+
 	"github.com/usa4ev/gotracer/internal/mongo/connector"
 	"github.com/usa4ev/gotracer/internal/mongo/mongohook"
 	"github.com/usa4ev/gotracer/internal/mongo/provider"
@@ -27,25 +27,33 @@ func main(){
 
 	flag.Parse()
 
-	// Read url list from file 
-	resources,err := resources.ReadResources(*resourcePath)
-	if err != nil{
-		log.Fatal(err)
-	}
-
-	tracker := tracer.New(resources)
-	
 	// Create logger
 	logger := logrus.New()
 	logger.Formatter = &logrus.JSONFormatter{
 		DisableTimestamp: true,
 	}
 
+	// Read url list from file 
+	resources,err := resources.ReadResources(*resourcePath)
+	if err != nil{
+		logger.Fatal(err)
+	}
+
+	tracker := tracer.New(resources)
+
 	// Connect to mongo
 	conn,err := connector.New(*mongouri)
 	if err != nil{
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
+
+	defer func(){
+		ctx, _ := context.WithTimeout(context.Background(), 10 * time.Second)
+		err := conn.Disconnect(ctx)
+		if err != nil{
+			logger.Errorf("failed to close mongo connection: %v", ctx)
+		}
+	}()
 
 	// Create hook that writes logs to mongo
 	hook := mongohook.New(conn)
@@ -67,13 +75,13 @@ func main(){
 
 		if err := srv.Shutdown(context.Background()); err != nil {
 			// failed to close listener
-			log.Printf("HTTP server Shutdown: %v", err)
+			logger.Infof("HTTP server Shutdown: %v", err)
 		}
 
-		log.Printf("graceful shutdown, got call: %v\n", call.String())
+		logger.Errorf("graceful shutdown, got call: %v\n", call.String())
 	}()
 
 	if err := srv.ListenAndServe(); err != nil{
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
 }
